cfa9afbfbd11574d732f8e40e0590b88d395184c
W1RDy <ivan.alchin5@mail.ru>
2024-04-19 12:47:35 +0500

    Update scale changer

--- Assets/Scripts/AdditiveScripts/ScaleChanger.cs
+++ Assets/Scripts/AdditiveScripts/ScaleChanger.cs
@@ -2,21 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.Linq;
 
 public class ScaleChanger : MonoBehaviour
 {
     [SerializeField] RectTransform[] _transforms;
     [SerializeField] float _scaleChangeValue;
 
+    private List<RectTransform> _allTransforms = new List<RectTransform>();
+    private List<Vector3> _previousScales = new List<Vector3>();
+    private List<Vector3> _previousPositons = new List<Vector3>();
+
 #if UNITY_EDITOR
     [ContextMenu("ChangeScalesWithProportions")]
     public void ChangeScalesWithSavingProportions()
     {
-        foreach (var transform in _transforms)
+        if (_allTransforms.Count == 0)
         {
-            transform.localScale = transform.localScale * _scaleChangeValue;
+            foreach (var transform in _transforms)
+            {
+                var objTransforms = transform.GetComponentsInChildren<RectTransform>();
+                _allTransforms.Union(objTransforms);
+            }
+        }
+
+        Undo.RecordObjects(_allTransforms.ToArray(), "change scale and positions transform");
+        foreach (var transform in _allTransforms)
+        {
+            _previousScales.Add(transform.sizeDelta);
+            _previousPositons.Add(transform.localPosition);
+
+            transform.sizeDelta = transform.sizeDelta * _scaleChangeValue;
             transform.localPosition = transform.localPosition * _scaleChangeValue;
         }
     }
+#endif
+
+#if UNITY_EDITOR
+
+    [ContextMenu("ReturnScales")]
+    public void ReturnChanges()
+    {
+        if (_previousScales.Count == 0) throw new System.ArgumentOutOfRangeException("You should change scales first!");
+
+        for (int i = 0; i < _previousScales.Count; i++)
+        {
+            _allTransforms[i].localScale = _previousScales[i];
+            _allTransforms[i].localPosition = _previousPositons[i];
+        }
+    }
+
 #endif
 }
