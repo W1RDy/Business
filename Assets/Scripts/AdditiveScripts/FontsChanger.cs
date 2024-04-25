95b9420480bd9d8bf0312fcf131ce79c03dfd7d2
W1RDy <ivan.alchin5@mail.ru>
2024-04-25 16:56:30 +0500

    Change fonts

---
+++ Assets/Scripts/AdditiveScripts/FontsChanger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+public class FontsChanger : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _fontsObjs;
+    [SerializeField] private TMP_FontAsset _newFont;
+
+    private List<TextMeshProUGUI> _texts = new List<TextMeshProUGUI>();
+
+    [ContextMenu("Change Fonts")]
+    public void ChangeFonts()
+    {
+        _texts.Clear();
+        foreach (var fontObj  in _fontsObjs)
+        {
+            if (fontObj.TryGetComponent<TextMeshProUGUI>(out var text)) _texts.Add(text);
+        }
+
+        Undo.RecordObjects(_texts.ToArray(), "Change fonts");
+        foreach (var text in _texts)
+        {
+            text.font = _newFont;
+        }
+    }
+}
